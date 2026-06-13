b9af61cf64183e2c4e1e1a6d0ed8d99e0d37b515
francis_85a <franperezcid85@gmail.com>
2026-06-13 23:08:54 +0200

    test(Test): Añadir casos test

---
+++ src/test/java/edu/teamrocket/cotxox/ConductorTest.java
@@ -0,0 +1,33 @@
+package edu.teamrocket.cotxox;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import edu.teamrocket.cotxox.conductor.Conductor;
+import org.junit.jupiter.api.Test;
+
+public class ConductorTest {
+
+	@Test
+	public void ConstructorYgetConductorTest() {
+		String nombre = "Samantha";
+		Conductor conductor = new Conductor(nombre);
+		assertEquals(nombre, conductor.getNombre());
+	}
+
+	@Test
+	public void ValoracionMediaTest() {
+		String nombre = "Samantha";
+		Conductor conductor = new Conductor(nombre);
+		conductor.setValoracion((byte) 5);
+		assertEquals(1, conductor.getNumeroValoraciones(), 0);
+		assertEquals(5, conductor.getValoracion(), 0.1);
+
+		int numeroValoraciones = 0;
+		for (int valoracion = 0; valoracion <= 5; valoracion++) {
+			conductor.setValoracion((byte) valoracion);
+			numeroValoraciones++;
+		}
+		assertEquals(numeroValoraciones + 1, conductor.getNumeroValoraciones(), 0);
+		assertEquals(2.875, conductor.getValoracion(), 0.1);
+	}
+}
